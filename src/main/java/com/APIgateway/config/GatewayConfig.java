9fc43d3501c69d1ff182d0a4254b6bd0f46d603e
Sunny Lee <86199470+sunnnycode@users.noreply.github.com>
2024-11-15 13:43:28 +0900

    feat: brand의 store, product API 라우터 코드 추가

--- src/main/java/com/APIgateway/config/GatewayConfig.java
+++ src/main/java/com/APIgateway/config/GatewayConfig.java
@@ -18,13 +18,49 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
 
+                // Brand - store
                 .route("store-owner-register", predicateSpec -> predicateSpec
                         .path("/api/store/owner/register")
                         .filters(gatewayFilterSpec -> gatewayFilterSpec
-                                .filter(jwtAuthorizationFilter)  // JwtAuthorizationFilter에서 토큰을 추출하여 헤더에 추가
+                                .filter(jwtAuthorizationFilter)
                         )
-                        .uri("lb://BRAND") // 서비스 이름으로 포워딩 (Eureka 서비스 레지스트리)
+                        .uri("lb://BRAND")
                 )
+
+                .route("store-update-status", predicateSpec -> predicateSpec
+                        .path("/api/store/master/{storeId}/status")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://BRAND")
+                )
+
+
+                // Brand - product
+                .route("product-register-owner", predicateSpec -> predicateSpec
+                        .path("/api/product/owner/{storeId}")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://BRAND")
+                )
+
+                .route("product-update-owner", predicateSpec -> predicateSpec
+                        .path("/api/product/owner/{storeId}/{productCode}")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://BRAND")
+                )
+
+                .route("product-delete-owner", predicateSpec -> predicateSpec
+                        .path("api/product/owner/{storeId}/{productCode}")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://BRAND")
+                )
+
                 // Swagger UI 라우팅
                 .route("swagger-ui", predicateSpec -> predicateSpec
                         .path("/swagger-ui/**", "/v3/api-docs/**")
@@ -32,4 +68,5 @@ public class GatewayConfig {
                 )
                 .build();
     }
+
 }
