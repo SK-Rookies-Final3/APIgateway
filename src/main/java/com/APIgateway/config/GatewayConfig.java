0269afac21877669ac16589476c6921df463fdcb
woo <clwkwhdk@gmail.com>
2024-11-28 17:12:15 +0900

    "feat: 장바구니 라우팅코드 추가"

--- src/main/java/com/APIgateway/config/GatewayConfig.java
+++ src/main/java/com/APIgateway/config/GatewayConfig.java
@@ -223,6 +223,47 @@ public class GatewayConfig {
                         .uri("lb://BRAND")
                 )
 
+                // 장바구니 관련 라우트, JWT 인증 필터 적용
+
+                .route("cart-items", predicateSpec -> predicateSpec
+                        .path("/api/cart/items")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://CART")
+                )
+
+                .route("cart-item-by-productCode", predicateSpec -> predicateSpec
+                        .path("/api/cart/items/{productCode}")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://CART")
+                )
+
+                .route("custom-cart-items", predicateSpec -> predicateSpec
+                        .path("/api/cart/custom/items")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://CART")
+                )
+
+                .route("update-cart-title", predicateSpec -> predicateSpec
+                        .path("/api/cart/custom/updateTitle")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://CART")
+                )
+
+                .route("custom-cart-item-by-productCode", predicateSpec -> predicateSpec
+                        .path("/api/cart/custom/items/{productCode}")
+                        .filters(gatewayFilterSpec -> gatewayFilterSpec
+                                .filter(jwtAuthorizationFilter)
+                        )
+                        .uri("lb://CART")
+                )
 
 
                 // Swagger UI 라우팅
